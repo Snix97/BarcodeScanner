2df876833ad646fc38250ca2378d9fa43adbb8be
Claire Roughan <ClaireRoughan.Dev@gmail.com>
2025-03-07 14:53:35 +0000

    Project setup

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Claire Roughan on 07/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarcodeScannerTests/BarcodeScannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerTests.swift
+//  BarcodeScannerTests
+//
+//  Created by Claire Roughan on 07/03/2025.
+//
+
+import Testing
+@testable import BarcodeScanner
+
+struct BarcodeScannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
