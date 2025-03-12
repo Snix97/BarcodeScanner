c45ed198875064652d4b162b81817f234b019b2c
Claire Roughan <ClaireRoughan.Dev@gmail.com>
2025-03-12 17:04:18 +0000

    Create separate Alert file to clean up the error handling code from being inside the BarcodeScannerView.  The alert file consists of an AlertItem and an AlertContext  to hold the different alert msgs you need to show

--- BarcodeScanner/BarcodeScannerView.swift
+++ BarcodeScanner/BarcodeScannerView.swift
@@ -5,28 +5,6 @@
 //  Created by Claire Roughan on 07/03/2025.
 //
 
-import SwiftUI
-
-//The Identifiable protocol in SwiftUI allows you to add a unique object identity
-struct AlertItem: Identifiable {
-    let id = UUID()
-    let title: String
-    let message: String
-    let dismissbutton: Alert.Button
-}
-
-//Helper to make it easy for call specific alerts
-struct AlertContext {
-    //Create our different alertItems
-    static let invalidDeviceInput = AlertItem(title: "Invalid Device Input",
-                                              message: "Camera problems? - Unable to capture input",
-                                              dismissbutton: .default(Text("OK")))
-    
-    static let invalidScannedType = AlertItem(title: "Invalid Scanned Type",
-                                              message: "The item scanned is not valid. This app scans EAN-6 and EAN-13 ",
-                                              dismissbutton: .default(Text("OK")))
-    
-}
 
 struct BarcodeScannerView: View {
     
